ad41534b9e94dd477862352d610044990174e1f0
Liam Naylor <liam@liamnaylor.com>
2023-06-06 07:12:11 -0700

    Add public key

--- web/src/components/PaymentForm.js
+++ web/src/components/PaymentForm.js
@@ -1,3 +1,10 @@
 export default function PaymentForm() {
 
+  const STRIPE_PUBLIC_KEY = 'pk_test_51MilMTB7m5Agj7M4QVdcEmAwLcW8vLpS6HeU54QXyUK6WiOLNRTHqzFt6kvIl47JFW1h2qMpQS16HVl5gDncu6Z800Haj5nypi';
+
+  return (
+    <div>
+
+    </div>
+  )
 };
