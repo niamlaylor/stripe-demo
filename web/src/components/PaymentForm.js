a31fbabe61f89eb37ef6d7fced0f38543ce2bb88
Liam Naylor <liam@liamnaylor.com>
2023-06-06 07:56:32 -0700

    Add card options obj

--- web/src/components/PaymentForm.js
+++ web/src/components/PaymentForm.js
@@ -2,6 +2,26 @@ import { useState } from 'react';
 import axios from 'axios';
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 
+const CARD_OPTIONS = {
+  iconStyle: 'solid',
+  style: {
+    base: {
+      iconColor: '#c4f0ff',
+      color: '#fff',
+      fontWeight: 500,
+      fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+      fontSize: '16px',
+      fontSmoothing: 'antialiased',
+      ':- webkit-autofill': { color: '#fce883' },
+      '::placeholder': { color: '#87bbfd' } 
+    },
+    invalid: {
+      iconColor: '#ffc7ee',
+      color: '#ffc7ee'
+    }
+  }
+}
+
 export default function PaymentForm() {
   const [success, setSuccess] = useState(false);
 
@@ -20,7 +40,7 @@ export default function PaymentForm() {
         const {id} = paymentMethod
         const response = await axios.post('http://localhost:3000/', {
           amount: 1000,
-          id: '1'
+          id
         })
         if (response.data.success) {
           console.log("Success");
@@ -36,8 +56,21 @@ export default function PaymentForm() {
   }
 
   return (
-    <div>
-
-    </div>
+    <>
+      {!success ? 
+        <form onSubmit={handleSubmit}>
+          <fieldset className="FormGroup">
+            <div className="FormRow">
+              <CardElement options={CARD_OPTIONS}/>
+            </div>
+          </fieldset>
+          <button>Pay now</button>
+        </form>
+        :
+        <div>
+          <h2>Your order is on its way!</h2>
+        </div>
+      }
+    </>
   )
 };
