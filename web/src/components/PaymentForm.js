b1cfa23cfda512e0a6c48f215851ba6e43df14f1
Liam Naylor <liam@liamnaylor.com>
2023-06-06 07:32:39 -0700

    Add handleSubmit

--- web/src/components/PaymentForm.js
+++ web/src/components/PaymentForm.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+
 export default function PaymentForm() {
+  const [success, setSuccess] = useState(false);
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const {error, paymentMethod} = stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+  };
+
   return (
     <div>
-      
+
     </div>
   )
 };
